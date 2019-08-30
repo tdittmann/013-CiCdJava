9704c87f51a3012f02ceb6df5af3e4b049dcf838
Timo Dittmann <timo.dittmann@kaufland.com>
2019-08-30 12:52:37 +0200

    Initial commit

--- src/main/java/com/altoros/training/lab/CiTravisLabApplication.java
+++ src/main/java/com/altoros/training/lab/CiTravisLabApplication.java
@@ -2,7 +2,6 @@ package com.altoros.training.lab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class CiTravisLabApplication {
